da87f865d17555c674b5c1eacd5815b07a496381
geog4046instructor <geog4046instructor@gmail.com>
2021-01-10 21:32:34 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+/*
+  Calculates the distance between two points on Earth. Accepts two 
+  pairs of lat/lon coordinates in decimal degrees as input and returns 
+  the distance in km.
+  
+  The spherical law of cosines formula is from https://www.movable-type.co.uk/scripts/latlong.html.
+
+  Example input URL: 
+  https://8rwlo.sse.codesandbox.io/?latA=30&lonA=-90&latB=30.45&lonB=-91.187
+
+  Location names: New Orleans and Baton Rouge
+
+  Output: distance: 124.53850456983854
+*/
+var http = require("http");
+http
+  .createServer(function (request, response) {
+    // Read the URL used to contact the web service and extract the latitude and longitude values, saving them each to a variable
+    var requestUrl = new URL("http://" + request.headers.host + request.url);
+    var latA = requestUrl.searchParams.get("latA");
+    var lonA = requestUrl.searchParams.get("lonA");
+    var latB = requestUrl.searchParams.get("latB");
+    var lonB = requestUrl.searchParams.get("lonB");
+
+    // Use the spherical law of cosines formula to calculate distance along the surface of a sphere. It is not the most accurate method for Earth, but it is good enough.
+    const φ1 = (latA * Math.PI) / 180;
+    const φ2 = (latB * Math.PI) / 180;
+    const Δλ = ((lonB - lonA) * Math.PI) / 180;
+    const R = 6371; // Earth's radius in km
+    const d =
+      Math.acos(
+        Math.sin(φ1) * Math.sin(φ2) + Math.cos(φ1) * Math.cos(φ2) * Math.cos(Δλ)
+      ) * R;
+
+    // Output the calculated distance value to the client and complete the execution of the program.
+    response.write("{distance: " + d + "}");
+    response.end();
+  })
+  .listen(8080);
